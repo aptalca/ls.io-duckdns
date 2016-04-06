d5a356eecc23e809fdcc642df1a0955f2dc70027
aptalca <thedjinn@yahoo.com>
2016-04-06 18:02:45 -0400

    Update 40_initial_duckdns_update.sh

--- init/40_initial_duckdns_update.sh
+++ init/40_initial_duckdns_update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-rm -f /defaults/duck.conf
   #Check to make sure the subdomain and token are set
 if [ -z "$SUBDOMAINS" ] || [ -z "$TOKEN" ]; then
   echo "Please pass both your subdomain(s) and token as environment variables in your docker run command. See docker info for more details."
